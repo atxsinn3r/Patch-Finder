a795b0ed318e5a88f1db18a2a0ba21ab8150e475
Sam James <sam@gentoo.org>
2023-04-02 05:11:43 +0100

    Handle Fixnum removal (ruby 3.2 compat)
    Ruby 3.2 removes the long-deprecated Fixnum class. We should just use
    the generic Integer instead.

--- lib/patch_finder/engine/msu/google.rb
+++ lib/patch_finder/engine/msu/google.rb
@@ -74,7 +74,7 @@ module PatchFinder
         # Searches the Google API.
         #
         # @param opts [Hash]
-        # @option opts [Fixnum] :starting_index
+        # @option opts [Integer] :starting_index
         # @option opts [String] :keyword
         # @return [Hash] JSON
         def search(opts = {})
@@ -122,7 +122,7 @@ module PatchFinder
         # Returns totalResults
         #
         # @param j [Hash] JSON response.
-        # @return [Fixnum]
+        # @return [Integer]
         def get_total_results(j)
           j['queries']['request'].first['totalResults'].to_i
         end
@@ -130,7 +130,7 @@ module PatchFinder
         # Returns startIndex
         #
         # @param j [Hash] JSON response.
-        # @return [Fixnum]
+        # @return [Integer]
         def get_next_index(j)
           j['queries']['nextPage'] ? j['queries']['nextPage'].first['startIndex'] : 0
         end
